455a396735c076af2e507eb32a2511782225317b
Miguel-Val <chahpo@gmail.com>
2023-10-09 04:20:03 -0400

    Finished for now

--- index.js
+++ index.js
@@ -1,7 +1,21 @@
-const password = document.querySelector('.Password');
-const passwordConfirm = document.querySelector('.passwordConfirm');
-console.log(password);
-// function passwordCheck () {
-    
-// }
-// passwordCheck()
+const password = document.querySelector("#Password");
+const passwordConfirm = document.querySelector("#passwordConfirm");
+
+function checkPasswords() {
+    let passwordValue = password.value;
+    let confirmValue = passwordConfirm.value;
+
+    if (!passwordValue) {
+        password.style.borderColor = "none";
+        passwordConfirm.style.borderColor = "none";
+    } else if (confirmValue !== passwordValue) {
+        password.style.borderColor = "red";
+        passwordConfirm.style.borderColor = "red";
+    } else {
+        password.style.borderColor = "green";
+        passwordConfirm.style.borderColor = "green";
+    }
+}
+
+password.addEventListener("input", checkPasswords);
+passwordConfirm.addEventListener("input", checkPasswords);
